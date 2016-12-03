ed308b6f55b13ef3f4f5b2a557c77e6f4c205109
Auskennfuchs <auskennfuchs@auskennfuchs.de>
2016-12-04 00:19:36 +0100

    Tutorial1 finish

---
+++ SharpDXTutorial/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SharpDXTutorial {
+    public static class MainProgram {
+
+        [STAThread]
+        static void Main() {
+            var renderer = new Renderer();
+            var form = new TutorialForm(renderer);
+            form.Text = "SharpDXTutorial";
+            form.ClientSize = new System.Drawing.Size(1280, 720);
+
+            renderer.Init(form);
+            renderer.Run();
+            renderer.Dispose();
+        }
+    }
+}
---
+++ SharpDXTutorial/RenderForm.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace SharpDXTutorial {
+    abstract class RenderForm : Form{
+
+        public Renderer Renderer {
+            get; private set;
+        }
+
+        public RenderForm(Renderer renderer) {
+            Renderer = renderer;
+        }
+
+        public abstract void RenderLoop();
+
+        public abstract void ClearResources();
+
+        public virtual void InitScene() { }
+    }
+}
